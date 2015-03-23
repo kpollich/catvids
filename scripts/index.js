3c42d720b97d823b6ef3f893c1564c5512509c3d
Kyle Pollich <kpollich1@gmail.com>
2015-03-23 13:36:09 -0400

    Cat heads rotate on search button click. removed old js file. Added jqueryrotate to scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-console.log("Javascript");
+$(document).ready(function() {
+	$("#submit-search").on("click", function(event) {
+		var angle = 0;
+		setInterval(function(){
+      		angle+=3;
+     		$(".cat-head").rotate(angle);
+},10);
+	});
+});
