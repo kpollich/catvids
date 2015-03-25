ac4a7ff55a686b561af4e04bac0d8426661c2863
Kyle Pollich <kpollich1@gmail.com>
2015-03-25 09:50:18 -0400

    Added post video page

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		function() {
 			$(this).addClass("search-button-shadow");
 		},
-		functon() {
+		function() {
 			$(this).removeClass("search-button-shadow");
 		});*/
 
