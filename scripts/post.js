5465c55c9977022d142395e7585b3cd0cacc29c5
Kyle Pollich <kpollich1@gmail.com>
2015-04-22 09:55:34 -0400

    Post video feature working

--- scripts/post.js
+++ scripts/post.js
@@ -1,31 +1,22 @@
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
-
-
 $(document).ready( function() {
-	$("#post-video-form").submit( function(e) {
-		e.preventDefault();
-		var data = JSON.stringify($(this).serializeObject());
-		$("#result").text(data);
 
-		//Do API call with JSON data here
+    $('#post-video-form').submit(function(e) {
 
-		//Redirect user after clicking submit
-		//NOTE: This isn't a true form submit (there is no action page required)
-		//We're really just shuffling JSON around 
-	});
+    e.preventDefault(); //STOP default action
+
+    console.log("submitted");
+
+    var postData = $(this).serializeArray();
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://floyd.cs.millersville.edu:8080/addVideo',
+        data: postData,
+        dataType: 'json',
+        complete: function(json) {
+            alert("Hey! Purr-fect video!");
+            window.location.href = "../index.html";
+          }
+        });
+    });
 });
