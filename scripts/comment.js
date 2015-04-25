3f3d55b60101cb79005d2d762d6c80dc91cb0bcd
Kevin Fisher <kevincliffordfisher@gmail.com>
2015-04-25 15:42:39 -0400

    super neat stuff

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
 
-	var vidTitle = $.url().param("vidID");
-	console.log(vidTitle);
-	$("#video-title").text(vidTitle);
+	var api = "http://192.168.0.5:8080/getVideoById?catVidId="
+
+	//grab the url query string and parse it for the Vid ID
+	var pathname = $(location).attr("search");
+	console.log(pathname);
+	/*if(pathname == ""){
+		console.error("haha wat");
+	}*/
+	var vidId = pathname.substring(pathname.lastIndexOf("=")+1);
+	console.log(" vid ID: " +vidId);
+	if (vidId == -1 || vidId == ""){
+		console.log("Hey some shit went wrong I guess.");
+	
+	
+	}else{
+	document.title = vidId + " " + document.title;
 
 
+	$("#video-title").text("Video #"+vidId);
+	}
 });
